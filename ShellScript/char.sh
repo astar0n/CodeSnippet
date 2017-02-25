59983698155a4740f1fc6a2297fbde38ac5570af
astar0n <w3bcode@gmail.com>
2017-02-25 09:24:28 +0530

    Modified checking

--- ShellScript/char.sh
+++ ShellScript/char.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-for i in {181..240}
+for i in {61..120}
 do
 grep --color='auto' -P -Hn "[\x80-\xFF]" "$i.txt"
+grep --color='auto' -P -Hn "ﬂ" "$i.txt"
+grep --color='auto' -P -Hn "[‘’“”]" "$i.txt"
+grep --color='auto' -P -Hn "[{}']" "$i.txt"
+#grep --color='auto' -P -Hn ".I" "$i.txt"
 done
 
