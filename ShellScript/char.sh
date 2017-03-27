4335b123532cad10e55462cad9fa081326735fa3
astar0n <w3bcode@gmail.com>
2017-03-27 19:00:17 +0530

    Some changes made for new characters

--- ShellScript/char.sh
+++ ShellScript/char.sh
@@ -7,5 +7,7 @@ grep --color='auto' -P -Hn "ﬂ" "$i.txt"
 grep --color='auto' -P -Hn "[‘’“”]" "$i.txt"
 grep --color='auto' -P -Hn "[{}']" "$i.txt"
 #grep --color='auto' -P -Hn ".I" "$i.txt"
+#[^\x00-\x7F]
+grep --color='auto' -P -Hn "[^\x00-\x7F]" "$i.txt"
 done
 
